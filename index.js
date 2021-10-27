b735139c9fd2e05f71390c3149b6cbcc393e1572
Theunis Knoetze <theunis.knoetze@gmail.com>
2021-10-27 14:48:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const bodyEl = document.querySelector("body");
+
+const buttonEl = document.createElement("button");
+bodyEl.append(buttonEl);
+
+function updateBodyStyle() {
+    bodyEl.style.backgroundColor = "antiqueWhite";
+    bodyEl.style.display = "grid";
+    bodyEl.style.placeItems = "center";
+}
+
+
+function updateButtonColour(colour) {
+    buttonEl.style.backgroundColor = colour;
+}
+
+function updateButtonStyle() {
+    buttonEl.style.height = "100px";
+    buttonEl.style.width = "100px";
+}
+
+function updateButton(colour) {
+  updateButtonColour(colour);
+  updateButtonStyle();
+}
+
+function getRandomColour() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function buttonUpdateStyle() {
+    updateButton(getRandomColour());
+    updateBodyStyle();
+}
+
+buttonUpdateStyle();
+
+buttonEl.addEventListener("click", buttonUpdateStyle);
